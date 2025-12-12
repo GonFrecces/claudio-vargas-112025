3047f5ea5051fcbf1ac171c791f2ffd592009f7a
GonFrecces <54601577+GonFrecces@users.noreply.github.com>
2025-12-12 20:14:21 -0300

    feat(change tailwind styles)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
   resolve : {
     alias: {
-      '@': '/src'
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
